9eadbd5bb5d116b8451d14ff7b45668fc07d3563
VED PRAKASH <153211377+VED-PRAKA5H@users.noreply.github.com>
2024-08-16 22:43:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,21 @@
-from flask import Flask, request, jsonify, render_template
-
-# Create the app object
-app = Flask(__name__)
-
-
-# importing function for calculations
+from flask import Flask, request, render_template
 from basic_calculator_function import basic_calculator
 
-# Define calculator
+app = Flask(__name__)
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/predict',methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
-
     a = request.form['a']
     b = request.form['b']
     operation = str(request.form['operation'])
 
-    result = basic_calculator(a,b,operation)
+    result = basic_calculator(a, b, operation)
 
     return render_template('index.html', prediction_text=str(result))
-
 
 if __name__ == "__main__":
     app.run(debug=True)
