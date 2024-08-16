45461eac24eb4f5702b217c19784148501d2feea
VED PRAKASH <153211377+VED-PRAKA5H@users.noreply.github.com>
2024-08-16 22:43:59 +0530

    Update basic_calculator_function.py

--- basic_calculator_function.py
+++ basic_calculator_function.py
@@ -1,21 +1,18 @@
-def basic_calculator(a,b,operation):
-
-  if (a.isnumeric() & b.isnumeric()):
-    a=float(a)
-    b=float(b)
-    if operation == "add":
-      result = a + b
-    elif operation == "subtract":
-      result = a - b
-    elif operation == "divide":
-      result = a / b
-    elif operation == "multiply":
-      result = a * b
+def basic_calculator(a, b, operation):
+    if (a.isnumeric() and b.isnumeric()):
+        a = float(a)
+        b = float(b)
+        if operation == "add":
+            result = a + b
+        elif operation == "subtract":
+            result = a - b
+        elif operation == "divide":
+            result = a / b
+        elif operation == "multiply":
+            result = a * b
+        else:
+            result = "Operations supported: add, subtract, divide, multiply only"
     else:
-      result = "Operations supported: add, subtract, divide, multiple only"
+        result = "Please enter a valid number for a & b"
     
-  else:
-    result = "Please enter a valid number for a & b"
-    
-
-  return result
+    return result
